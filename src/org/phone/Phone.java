8e80b61b020103d39ac9807e8adc6b8721927a5b
deepandon <105847932+deepandon@users.noreply.github.com>
2022-05-19 13:56:03 +0530

    final commit
    try it

--- src/org/phone/Phone.java
+++ src/org/phone/Phone.java
@@ -9,13 +9,16 @@ public class Phone {
 	private void phoneInfo(String Name) {
 		System.out.println(Name);
 	}
+	private void phoneInfo(String Name,int num){
+		System.out.println(Name+num);
+	}
 
 	public static void main(String[] args) {
 		
 		Phone p=new Phone();
 		p.phoneInfo(289);
 		p.phoneInfo("Note 5 Pro");
-
+                p.phoneInfo("emi"5000);
 	}
 
 }
